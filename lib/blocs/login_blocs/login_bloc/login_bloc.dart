34a8c3b1df1f42d3373cc1e47259b6c2a15dadc3
Gis <carpinteiro.gisel@gmail.com>
2023-09-23 23:58:35 -0600

    login firebase added

--- lib/blocs/login_blocs/login_bloc/login_bloc.dart
+++ lib/blocs/login_blocs/login_bloc/login_bloc.dart
@@ -2,6 +2,7 @@ import 'package:bloc/bloc.dart';
 import 'package:equatable/equatable.dart';
 import 'package:flutter/material.dart';
 import 'package:vistas_amatista/resources/custom_widgets/msos_snackbar.dart';
+import 'package:firebase_auth/firebase_auth.dart';
 
 part 'login_event.dart';
 part 'login_state.dart';
@@ -12,7 +13,25 @@ class LoginBloc extends Bloc<LoginEvent, LoginState> {
       String email = event.email;
       String password = event.password;
       print("El correo es ${email}, y la contraseña es ${password}");
-      emit(const LoginGetAccount$(email: "ejemlo@gmail.com", password: "ContraseñaSuperSegura"));
+      signInWithEmailPassword(email, password);
+      emit(const LoginGetAccount$(
+          email: "ejemlo@gmail.com", password: "ContraseñaSuperSegura"));
     });
+  }
+
+  signInWithEmailPassword(email, psw) async {
+    try {
+      final credential = await FirebaseAuth.instance.signInWithEmailAndPassword(
+          //email: "carpinteiro.gisel@gmail.com", password: "Amatista1234");
+          email: email,
+          password: psw);
+      print(credential);
+    } on FirebaseAuthException catch (e) {
+      if (e.code == 'user-not-found') {
+        print('No user found for that email.');
+      } else if (e.code == 'wrong-password') {
+        print('Wrong password provided for that user.');
+      }
+    }
   }
 }
