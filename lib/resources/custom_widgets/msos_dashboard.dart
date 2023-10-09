7aec70213459fc79e4ef3d8f20f448104452cda8
Gis <a20110353@live.ceti.mx>
2023-10-08 23:18:44 -0600

    dashhboard jijiji

--- lib/resources/custom_widgets/msos_dashboard.dart
+++ lib/resources/custom_widgets/msos_dashboard.dart
@@ -2,6 +2,10 @@ import 'package:flutter/material.dart';
 import 'package:font_awesome_flutter/font_awesome_flutter.dart';
 import 'package:vistas_amatista/resources/colors/default_theme.dart';
 import 'package:vistas_amatista/resources/custom_widgets/msos_text.dart';
+import 'package:vistas_amatista/screens/alerts/alert_list.dart';
+import 'package:vistas_amatista/screens/groups/group_list.dart';
+import 'package:vistas_amatista/screens/home.dart';
+import 'package:vistas_amatista/screens/routines/routine_list.dart';
 
 class MSosDashboard extends StatelessWidget {
   const MSosDashboard({super.key});
@@ -10,63 +14,145 @@ class MSosDashboard extends StatelessWidget {
   Widget build(BuildContext context) {
     return Drawer(
       child: Padding(
-        padding: const EdgeInsets.fromLTRB(10, 20, 10, 20),
-        child: ListView(
+        padding: const EdgeInsets.fromLTRB(10, 60, 10, 20),
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.spaceBetween,
           children: [
-            const Row(
+            Column(
               children: [
-                SizedBox(width: 20),
-                FaIcon(FontAwesomeIcons.solidGem, color: MSosColors.amethyst),
-                SizedBox(width: 10),
-                MSosText(
-                  "AMATISTA",
-                  style: MSosText.wizardTitleStyle,
-                  textColor: MSosColors.black,
-                )
+                const Row(
+                  children: [
+                    SizedBox(width: 10),
+                    FaIcon(
+                      FontAwesomeIcons.solidGem,
+                      color: MSosColors.amethyst,
+                      size: 35,
+                    ),
+                    SizedBox(width: 10),
+                    MSosText(
+                      "AMATISTA",
+                      style: MSosText.wizardTitleStyle,
+                      textColor: MSosColors.black,
+                    ),
+                  ],
+                ),
+                const SizedBox(height: 25),
+                const Row(
+                  children: [
+                    SizedBox(width: 20),
+                    MSosText(
+                      "DASHBOARD",
+                      textColor: MSosColors.grayLight,
+                    ),
+                  ],
+                ),
+                const SizedBox(height: 10),
+                ListTile(
+                  leading: const Icon(Icons.home),
+                  title: const Text('Inicio'),
+                  onTap: () {
+                    Navigator.push(
+                      context,
+                      MaterialPageRoute(
+                          builder: (context) => const HomeScreen()),
+                    );
+                  },
+                ),
+                ListTile(
+                  leading: const Icon(Icons.alarm),
+                  title: const Text('Alertas'),
+                  onTap: () {
+                    Navigator.push(
+                      context,
+                      MaterialPageRoute(
+                          builder: (context) => const AlertSettingsScreen()),
+                    );
+                  },
+                ),
+                ListTile(
+                  leading: const Icon(Icons.group),
+                  title: const Text('Grupos'),
+                  onTap: () {
+                    Navigator.push(
+                      context,
+                      MaterialPageRoute(
+                          builder: (context) => const GroupListScreen()),
+                    );
+                  },
+                ),
+                ListTile(
+                  leading: const Icon(Icons.route_outlined),
+                  title: const Text('Rutinas'),
+                  onTap: () {
+                    Navigator.push(
+                      context,
+                      MaterialPageRoute(
+                          builder: (context) => const RoutineListScreen()),
+                    );
+                  },
+                ),
+                ListTile(
+                  leading: const Icon(Icons.gamepad),
+                  title: const Text('Activadores'),
+                  onTap: () {
+                    // TODO: Agregar ruta
+                  },
+                ),
+                ListTile(
+                  leading: const Icon(Icons.map),
+                  title: const Text('Mapa de Riesgos'),
+                  onTap: () {
+                    // TODO: Agregar ruta
+                  },
+                ),
               ],
             ),
-            const SizedBox(height: 10),
-            ListTile(
-              title: const Text('Inicio'),
-              onTap: () {
-                // Update the state of the app.
-                // ...
-              },
-            ),
-            ListTile(
-              title: const Text('Alertas'),
-              onTap: () {
-                // Update the state of the app.
-                // ...
-              },
-            ),
-            ListTile(
-              title: const Text('Grupos'),
-              onTap: () {
-                // Update the state of the app.
-                // ...
-              },
-            ),
-            ListTile(
-              title: const Text('Rutinas'),
-              onTap: () {
-                // Update the state of the app.
-                // ...
-              },
-            ),
-            ListTile(
-              title: const Text('Activadores'),
-              onTap: () {
-                // Update the state of the app.
-                // ...
-              },
-            ),
-            ListTile(
-              title: const Text('Mapa de Riesgos'),
-              onTap: () {
-                // Update the state of the app.
-                // ...
-              },
+            Column(
+              children: [
+                ListTile(
+                  shape: RoundedRectangleBorder(
+                    borderRadius: BorderRadius.circular(10),
+                  ),
+                  tileColor: MSosColors.blue,
+                  leading: const Icon(Icons.settings),
+                  iconColor: MSosColors.white,
+                  title: const Text('Configuración'),
+                  textColor: MSosColors.white,
+                  onTap: () {},
+                ),
+                const SizedBox(height: 20),
+                const Divider(),
+                ListTile(
+                  leading: const Image(
+                    image:
+                        AssetImage('lib/resources/assets/images/user_icon.png'),
+                  ),
+                  title: Container(
+                    alignment: Alignment
+                        .centerRight, // Alinea el contenido a la derecha
+                    child: const Row(
+                      mainAxisAlignment: MainAxisAlignment
+                          .spaceBetween, // Alinear el icono a la derecha
+                      children: [
+                        Column(
+                          crossAxisAlignment: CrossAxisAlignment.start,
+                          children: [
+                            Text('Ivanna Ramirez'),
+                            MSosText(
+                              "ivanna@gmail.com",
+                              size: 13,
+                              textColor: MSosColors.grayMedium,
+                            ),
+                            //Text('ivanna@gmail.com'),
+                          ],
+                        ),
+                        Icon(Icons
+                            .settings), // Cambia 'some_icon' por el icono que desees
+                      ],
+                    ),
+                  ),
+                ),
+              ],
             ),
           ],
         ),
