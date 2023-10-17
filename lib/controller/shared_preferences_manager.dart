d8755e3fa17142643ff6d20ce9cdcf9c0a3ad36d
Gis <carpinteiro.gisel@gmail.com>
2023-10-16 21:49:16 -0600

    bug

--- lib/controller/shared_preferences_manager.dart
+++ lib/controller/shared_preferences_manager.dart
@@ -2,6 +2,8 @@ import 'package:shared_preferences/shared_preferences.dart';
 import 'package:vistas_amatista/controller/group_controller.dart';
 import 'package:vistas_amatista/controller/alert_controller.dart';
 import 'package:vistas_amatista/controller/routines_controller.dart';
+import 'package:cloud_firestore/cloud_firestore.dart';
+import 'package:firebase_auth/firebase_auth.dart';
 
 /* About this Class: Shared Preferences is a library used to store data locally to retrieve it later when needed*/
 
@@ -15,13 +17,43 @@ class SharedPrefsManager {
   // This getter is the used to acces shared preferences AKA local data copies from all other places in the APP. That's why _sharedPrefs is an static variable.
   static SharedPreferences? get instance => _sharedPrefs;
 
+  static Future<List> getPeople() async {
+    List people = [];
+    FirebaseFirestore db = FirebaseFirestore.instance;
+    CollectionReference collectionReferenceUser = db.collection('user');
+    QuerySnapshot queryUser = await collectionReferenceUser.get();
+    queryUser.docs.forEach((document) {
+      people.add(document.data());
+    });
+    return people;
+  }
+
   // This method is used to get all the configurations from firebase to the shared preferences variables on the app
-  static Future<bool> restoreAllConfigsFromFirebase({String? password, String? email}) async {
+  static Future<bool> restoreAllConfigsFromFirebase(
+      {String? password, String? email}) async {
     // TODO: (Gisel) Ahora mismo no hacemos nada con la contraseña y el usuario pero todo lo de abajo marcado con "!" debería ser remplazado para provenir de Firebase a través de una consulta con el usuario y contraseña
     // * Antes este método se llamaba restoreAllConfigsFromLocal pero no tenía mucho sentido ya que eso no era eso lo que hacía y le agregué los parámetros que ahora mismo son opcionales para hacer pruebas pero deben ser required
-    String groupsData = await GroupController.getGroupsAsString(); // ! Replace with Firebase
-    String alertsData = await AlertController.getAlertsAsString(); // ! Replace with Firebase
-    String routinesData = await RoutineController.getRoutinesAsString(); // ! Replace with Firebase
+
+    // Obtén el usuario autenticado
+    final User user = FirebaseAuth.instance.currentUser!;
+
+    CollectionReference users = FirebaseFirestore.instance.collection('User');
+    List people = [];
+    FirebaseFirestore db = FirebaseFirestore.instance;
+    CollectionReference collectionReferenceUser = db.collection('user');
+    QuerySnapshot queryUser = await collectionReferenceUser.get();
+    queryUser.docs.forEach((document) {
+      people.add(document.data());
+      print(document.data());
+    });
+    print("firebase");
+
+    String groupsData =
+        await GroupController.getGroupsAsString(); // ! Replace with Firebase
+    String alertsData =
+        await AlertController.getAlertsAsString(); // ! Replace with Firebase
+    String routinesData = await RoutineController
+        .getRoutinesAsString(); // ! Replace with Firebase
 
     _sharedPrefs ??= await SharedPreferences.getInstance();
     await _sharedPrefs?.setString('groups', groupsData);
